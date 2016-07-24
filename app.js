2626794f5108da355e1001b4eacd8c3648c496fe
lkjoshi <lkjosi@gmail.com>
2016-07-24 22:50:41 +0530

    -- Bug fixes --

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 module.exports = function (url) {
     var urlParser = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
 
-    if(url) {
+    if(!url) {
         return [];
     } 
 
